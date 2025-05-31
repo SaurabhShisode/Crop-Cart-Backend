6e78327a2e34df0e0872f321369f4465489b2bab
SaurabhShisode <119099005+SaurabhShisode@users.noreply.github.com>
2025-05-31 14:31:15 +0530

    added quantity

--- models/Crops.js
+++ models/Crops.js
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
 
 const cropSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },    // unique id as string
+  id: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   price: { type: Number, required: true },
   availability: { type: String, required: true },
   image: { type: String },
   regionPincodes: [{ type: String, required: true }],
   type: { type: String, required: true },
+  quantity: { type: String, required: true }, // new field added
 });
 
 const Crop = mongoose.model('Crop', cropSchema);
