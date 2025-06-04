8488bd3466ed8a3704c233676af3d0e42fce676f
SaurabhShisode <119099005+SaurabhShisode@users.noreply.github.com>
2025-06-05 01:51:02 +0530

    Update farmerRoutes.js

--- routes/farmerRoutes.js
+++ routes/farmerRoutes.js
@@ -18,7 +18,7 @@ router.post('/crops', addCrop);
 router.get('/crops', getMyCrops);
 router.get('/earnings', getEarnings);
 router.get('/total-sold', getTotalSold);
-router.get('/stats', getAnalytics);
+router.get('/analytics', getAnalytics);
 router.delete('/crops/:id', protect, requireFarmer, deleteCrop);
 
 router.get('/orders', getMyOrders);
