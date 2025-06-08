0c9425269c27136a00bda38331c268f52fd0a82c
SaurabhShisode <119099005+SaurabhShisode@users.noreply.github.com>
2025-06-08 11:19:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // ===== Middleware =====
 app.use(cors({
-  origin: ['http://localhost:5173/', 'https://crop-cart-rose.vercel.app'],
+  origin: ['http://localhost:5173', 'https://crop-cart-rose.vercel.app'],
   credentials: true,
 }));
 app.use(express.json());
